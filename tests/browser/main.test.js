8b006fe19b8d3a11b823aeac0820d95e8b760d08
j-devel <j@w3reality.com>
2020-03-01 13:20:52 +0700

    FIXME: globals not defined when with `jest-puppeteer` and Node.js v10.13.0

--- tests/browser/main.test.js
+++ tests/browser/main.test.js
@@ -1,6 +1,16 @@
-console.log('beforeAll:', beforeAll); // FIXME not defined on x11
-console.log('test:', test); // FIXME not defined on x11
+
+console.log('beforeAll:', beforeAll); // FIXME globals not defined when with `jest-puppeteer` and Node.js v10.13.0
 
 test('foo', () => {
     expect(1).toBe(1);
 });
+
+// describe('Google', () => {
+//     beforeAll(async () => {
+//         await page.goto('https://google.com');
+//     });
+//
+//     it('should be titled "Google"', async () => {
+//         await expect(page.title()).resolves.toMatch('Google');
+//     });
+// });
