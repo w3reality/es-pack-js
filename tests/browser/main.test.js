d26609a70c70e1adc4aa0faaa2b4bea72a4e49b6
j-devel <j@w3reality.com>
2020-03-06 16:14:27 +0700

    waypoint: refactoring `testImport()` of browser tests

--- tests/browser/main.test.js
+++ tests/browser/main.test.js
@@ -15,6 +15,8 @@ const modEsmCompat = `${outDir}/${libName}.esm.compat.js`;
 const Server = require('./server');
 let server = null;
 
+let metaArgs = null;
+
 beforeAll(async () => {
     if (1) {
         console.error('!! skipping build !!');
@@ -25,6 +27,8 @@ beforeAll(async () => {
 
     server = await (new Server(outDir)).listen();
     console.log('server.port:', server.port);
+
+    metaArgs = [libobjName, outDir, server.port];
 });
 
 afterAll(async () => {
@@ -33,52 +37,7 @@ afterAll(async () => {
     server = null;
 });
 
-test('umd: tag', async () => units['umd-tag'](modUmd, libobjName, outDir, server.port));
-
-test('esm-compat: tag', async () => units['esm-compat-tag'](modEsmCompat, libobjName, outDir, server.port));
-
-// test('esm: load via static/dynamic `import`', async () => {
-//     const page = await browser.newPage();
-//     page.on('console', consoleObj => console.log(consoleObj.text())); // https://stackoverflow.com/questions/46198527/puppeteer-log-inside-page-evaluate
-//
-//     const html = htmlTemplate
-//         .replace('__TITLE__', 'static/dynamic import')
-//         .replace('__BODY__', `
-// <script type="module">
-//     window.foo = 42;
-//
-//     import Mod1 from './test-mod.esm.js';
-//     window.Mod1 = Mod1;
-//
-//     (async () => {
-//         const Mod2 = await import('./test-mod.esm.js');
-//         window.Mod2 = Mod2;
-//     })();
-// </script>
-//         `);
-//     fs.writeFileSync(`${outDir}/index-import.html`, html);
-//
-//     // NG per CORS
-//     // await page.goto(`file:${pathRelTests('browser/target/index-import.html')}`);
-//     //====
-//     await page.goto(`http://localhost:${server.port}/target/index-import.html`);
-//
-//     console.log('title:', await page.title());
-//
-//     let foo = await page.evaluate(() => window['foo']);
-//     expect(foo).toBe(42);
-//
-//     let Mod, ty;
-//
-//     // static import
-//     Mod = await page.evaluate(() => window['Mod1']);
-//     console.log('Mod1:', Mod);
-//     ty = typeof Mod;
-//     expect(ty === 'function' || ty === 'object').toBe(true);
-//
-//     // dynamic import
-//     Mod = await page.evaluate(() => window['Mod2']);
-//     console.log('Mod2:', Mod);
-//     ty = typeof Mod;
-//     expect(Mod.hasOwnProperty('default')).toBe(true);
-// });
+test('umd: tag', async () => units['umd-tag'](modUmd, ...metaArgs));
+test('esm: static import', async () => units['esm-import-static'](modEsm, ...metaArgs));
+test('esm: dynamic import', async () => units['esm-import-dynamic'](modEsm, ...metaArgs));
+test('esm-compat: tag', async () => units['esm-compat-tag'](modEsmCompat, ...metaArgs));
--- tests/browser/units.js
+++ tests/browser/units.js
@@ -38,13 +38,64 @@ const testTag = async (mod, libobjName, serveDir, port) => {
     fs.removeSync(copyPath);
 };
 
+const testImport = mode => async (mod, libobjName, serveDir, port) => {
+    expect(1).toBe(100); return; // !!!!!!!!!!!!!
+    // begin TODO *****************************
+
+    const page = await browser.newPage();
+    page.on('console', obj => console.log(obj.text())); // https://stackoverflow.com/questions/46198527/puppeteer-log-inside-page-evaluate
+
+    const html = htmlTemplate
+        .replace('__TITLE__', `${mode} import`)
+        .replace('__BODY__', `
+<script type="module">
+window.foo = 42;
+
+import Mod1 from './test-mod.esm.js';
+window.Mod1 = Mod1;
+
+(async () => {
+    const Mod2 = await import('./test-mod.esm.js');
+    window.Mod2 = Mod2;
+})();
+</script>
+        `);
+    fs.writeFileSync(`${outDir}/index-import.html`, html);
+
+    // NG per CORS
+    // await page.goto(`file:${pathRelTests('browser/target/index-import.html')}`);
+    //====
+    await page.goto(`http://localhost:${server.port}/target/index-import.html`);
+
+    console.log('title:', await page.title());
+
+    let foo = await page.evaluate(() => window['foo']);
+    expect(foo).toBe(42);
+    // end TODO *****************************
+
+    switch (mode) {
+        case 'static': {
+            const Mod = await page.evaluate(() => window['Mod1']);
+            console.log('static import - Mod:', Mod);
+            const ty = typeof Mod;
+            expect(ty === 'function' || ty === 'object').toBe(true);
+            break;
+        }
+        case 'dynamic': {
+            const Mod = await page.evaluate(() => window['Mod2']);
+            console.log('dynamic import - Mod:', Mod);
+            expect(Mod.hasOwnProperty('default')).toBe(true);
+            break;
+        }
+        default: throw `invalid mode: ${mode}`;
+    }
+};
+
 const units = {
-    'umd-tag': async (...args) => await testTag(...args),
-    'esm-import-static': async (mod) => {
-    },
-    'esm-import-dynamic': async (mod) => {
-    },
-    'esm-compat-tag': async (...args) => await testTag(...args),
+    'umd-tag': testTag,
+    'esm-import-static': testImport('static'),
+    'esm-import-dynamic': testImport('dynamic'),
+    'esm-compat-tag': testTag,
 };
 
 module.exports = { units };
