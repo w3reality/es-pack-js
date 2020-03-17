58101093c59562e10488d060c7cdb69032e0d4b7
j-devel <j@w3reality.com>
2020-03-17 12:46:08 +0700

    Remove non essential stuff

--- examples/test-webpack/es-pack.config.js
+++ examples/test-webpack/es-pack.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     onWebpackConfigCreated: config => {
-        config.output.library = 'FooMod';
         config['JustNotExist'] = 42; // `webpack()` should raise a `WebpackOptionsValidationError`
     },
 };
