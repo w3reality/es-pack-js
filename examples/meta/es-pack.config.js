fc5b7a086ad0c30045e9f5e39c17927b11281eb2
j-devel <j@w3reality.com>
2020-11-04 14:39:05 +0700

    Fix test:ex:meta

--- examples/meta/es-pack.config.js
+++ examples/meta/es-pack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     onBundle: (webpackConfig) => {
         // Keep async/await to work around "ReferenceError: regeneratorRuntime is not defined"
         for (let rule of webpackConfig.module.rules) {
-            if (rule.loader === 'babel-loader') {
+            if (rule.loader.endsWith('/babel-loader')) {
                 rule.options.presets = [['@babel/preset-env', {targets: {node: '10'}, modules: false}]];
             }
         }
