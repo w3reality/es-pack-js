77754f3e7c7117e43d55a7019701c8ed0b245366
j-devel <j@w3reality.com>
2020-12-01 15:02:43 +0700

    Refine error messages

--- src/meta/index.js
+++ src/meta/index.js
@@ -5,13 +5,17 @@ class Meta {
         const _console = console;
         _console.log.apply(_console, args);
     }
+
+    // https://github.com/w3reality/three-geo/issues/25#issuecomment-735853553
+    // FIXME: Not reliably working!!
     static isNodeJS() {
         return typeof __non_webpack_require__ !== 'undefined';
     }
+
     static async nodeRequire(_global, mod) { // {classic,es}-context agnostic wrapper
         const req = typeof _global.require === 'function' ? _global.require : null;
         const imp = typeof _global.import === 'function' ? _global.import : null;
-        if (!req && !imp) throw 'oops; maybe you should add `global.require = require;` in test script.';
+        if (!req && !imp) throw new Error('Neither NodeJS `require` or `import` is available. (If in test environment, maybe, you should add `global.require = require;`.');
 
         return req ? req(mod) : (await imp(mod)).default;
     }
@@ -35,7 +39,7 @@ class Delta {
         return this;
     }
     _checkInit() {
-        if (!this.perf) throw 'call `async .init()` first.';
+        if (!this.perf) throw new Error('Do call `async .init()` first.');
     }
     get() {
         this._checkInit();
