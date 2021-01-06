314672cf8adbb1418a7326c8d8727eef9a64a9cd
j-devel <j@w3reality.com>
2021-01-06 13:31:05 +0700

    Add 'test:ex:rustwasm' runner

---
+++ examples/rustwasm/tests/browser/main.test.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs-extra');
+const { Server } = require('../../../../src');
+
+const libName = 'add';
+const outDir = path.join(__dirname, '../../pkg-es-pack');
+const modPath = `${outDir}/${libName}.min.js`;
+
+const tmpModPath = `${__dirname}/__tmp.min.js`;
+
+let output;
+let server = null;
+beforeAll(async () => {
+    const serveDir = __dirname;
+    server = await (new Server(serveDir)).listen();
+
+    fs.copySync(modPath, tmpModPath);
+
+    const page = await browser.newPage();
+    await page.goto(`http://localhost:${server.port}/index.html`);
+
+    expect(await page.title()).toBe('tests');
+
+    await page.waitForFunction(`typeof window.output === "object"`);
+    output = await page.evaluate(() => window.output);
+
+    fs.removeSync(tmpModPath);
+});
+afterAll(async () => {
+    server.close();
+    server = null;
+});
+
+test('output', () => {
+    expect(typeof output).toBe('object');
+});
+test('misc', () => {
+    expect(output['results']).toEqual([4, 4, 4, 'object']);
+});
---
+++ examples/rustwasm/tests/node/main.test.js
@@ -0,0 +1,26 @@
+global.require = require;
+const path = require('path');
+
+const libName = 'add';
+const outDir = path.join(__dirname, '../../pkg-es-pack');
+
+const __modPath = `${outDir}/${libName}.min.js`;
+// const __modPath = `${outDir}/${libName}.js`; // dev !!
+
+const Add = require(__modPath);
+
+test('plain', async () => {
+    const wbg = await (new Add({nodejs: true})).init();
+    expect(wbg.add(2, 2)).toBe(4);
+});
+test('sugar', async () => {
+    const wbg = await Add.new({nodejs: true});
+    expect(wbg.add(2, 2)).toBe(4);
+});
+test('raw', async () => {
+    const mod = new Add({nodejs: true});
+    const wbg = await mod.init();
+    const wasm = mod.getWasm();
+    expect(wbg.add(2, 2)).toBe(4);
+    expect(typeof wasm['memory']).toBe('object');
+});
