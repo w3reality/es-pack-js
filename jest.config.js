6032b095e406f3c9aa2f525ede2016f4d0994474
j-devel <j@w3reality.com>
2021-01-07 14:20:21 +0700

    Workdaround jest's 5000ms timeout

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
     testMatch: ["<rootDir>/tests/node/**/*.test.js"],
     verbose: true, // display each test name
     silent: true, // supress console.log()
+    setupFilesAfterEnv: ["./jest.setup.js"],
 };
