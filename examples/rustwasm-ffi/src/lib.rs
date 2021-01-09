6173d758d5d4512b307e33581646993de00119f7
j-devel <j@w3reality.com>
2021-01-09 14:06:07 +0700

    Refactor ffi object creation with `create::<T>(ffi, item)`

--- examples/rustwasm-ffi/src/lib.rs
+++ examples/rustwasm-ffi/src/lib.rs
@@ -14,19 +14,25 @@ extern "C" {
     fn render(this: &MyClass) -> String;
 }
 
-#[wasm_bindgen]
-pub fn run(ffi: Object) -> Result<u32, JsValue> {
-    // `ffi.MyClass`
-    let my_class = Reflect::get(ffi.as_ref(), &"MyClass".into())?
+fn create<T: From<wasm_bindgen::JsValue>>(ffi: Object, item: &str) -> Result<T, JsValue> {
+    // `ffi[item]`
+    let clazz = Reflect::get(ffi.as_ref(), &item.into())?
         .dyn_into::<Function>()?;
 
-    //==== Given `my_class`, how to dynamically call `new MyClass()` in Rust?
-    // let x: MyClass = ??my_class??;
-    //==== hack: `MyClass.create()`
-    let x: MyClass = Reflect::get(my_class.as_ref(), &"create".into())?
+    //==== Given `clazz`, how to dynamically call `new T()` in Rust?
+    // let x: T = ??clazz??;
+    //==== hack: use custom 'static constructor' method: `T.create()`
+    let x: T = Reflect::get(clazz.as_ref(), &"create".into())?
         .dyn_into::<Function>()?
         .call0(&JsValue::undefined())?
         .into();
+
+    Ok(x)
+}
+
+#[wasm_bindgen]
+pub fn run(ffi: Object) -> Result<u32, JsValue> {
+    let x = create::<MyClass>(ffi, "MyClass").unwrap();
     
     assert_eq!(x.number(), 42);
     x.set_number(10);
