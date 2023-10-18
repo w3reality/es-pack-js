d1c0110804357d12dfb6e271eb595b32985ea725
j-devel <38098238+j-devel@users.noreply.github.com>
2023-10-18 11:35:26 +0700

    Workaround 'rustwasm-ffi/tests/node' issues on Node.js 10.x

--- examples/rustwasm-ffi/tests/node/main.test.js
+++ examples/rustwasm-ffi/tests/node/main.test.js
@@ -9,8 +9,10 @@ const __modPath = `${outDir}/${libName}.min.js`;
 
 const Mod = require(__modPath);
 
+if (process.version > 'v12.') { // !! FIXME - issues with Node.js 10.x - CompileError: AsyncCompile: Compiling wasm function #21:<?> failed: Invalid opcode (enable with --experimental-wasm-se) @+13578
 test('MyClass', async () => {
     const wbg = await Mod.create({nodejs: true});
     expect(typeof Mod.ffi).toBe('object'); // e.g. {"MyClass": [Function A]}
     expect(wbg.run(Mod.ffi)).toBe(10);
 });
+} // !!
