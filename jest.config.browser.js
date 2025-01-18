bb4a5885ab329568d1a863a0c4cc420c29daa2d6
j-devel <38098238+j-devel@users.noreply.github.com>
2025-01-18 15:42:43 +0700

    Update jest.config.browser.js - silent: false,

--- jest.config.browser.js
+++ jest.config.browser.js
@@ -1,6 +1,6 @@
 module.exports = {
     testMatch: ["<rootDir>/tests/browser/**/*.test.js"],
     verbose: true, // display each test name
-    silent: true, // supress console.log()
+    silent: false,// !! true, // supress console.log()
     setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.js"],
 };
